720434c9523cd9891383526d8b5b02cac4d65e2c
Ed Charbeneau <ed.charbeneau@gmail.com>
2020-08-14 12:16:52 -0400

    Working theme state

--- BlazingCoffee/Client/Program.cs
+++ BlazingCoffee/Client/Program.cs
@@ -1,5 +1,6 @@
 using BlazingCoffee.Services;
 using BlazingCoffee.Shared.Localization;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,6 +39,7 @@ namespace BlazingCoffee.Client
         {
             builder.Services.AddTelerikBlazor();
 
+            builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddHttpClient<PublicClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
             builder.Services.AddHttpClient("BlazingCoffee.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                             .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
